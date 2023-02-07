594ae19c9ae996d8240498f5530ff88e90ceba01
Elie BINGUE <eliebingue7@icloud.com>
2023-02-07 09:39:41 +0100

    mise à jour dropdown

--- src/Components/Dropdown.jsx
+++ src/Components/Dropdown.jsx
@@ -8,6 +8,7 @@ function Dropdown ({titre, description}) {
 	return (
 		<div className='dropdown' id={`dropdown${titre}`}>
 			<div className='top__dropdown'>
+				<p className='title__dropdown'>{titre}</p>
 				<span className={`arrow__dropdown ${open}`} onClick={() => setOpen(!open)}>
 					<img src={arrow} alt='Ouvrir cette liste'/>
 				</span>
